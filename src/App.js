ff3eebb44a22cdc2f66108106ff7e5ee0e84741a
Mauricio Cartagena <mc973220@gmail.com>
2020-11-26 19:19:36 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+
+import { BandAdd } from './componets/BandAdd';
+import { BandList } from './componets/BandList';
+
+
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', {
+    transports:['websocket']
+  });
+  return socket; 
+}
+
+function App() {
+
+  const [ socket ] = useState( connectSocketServer() );
+  const [ online, setOnline ] = useState(false);
+
+
+  useEffect(() => {
+    setOnline( socket.connected );
+  }, [ socket ]);
+
+  useEffect(() => {
+    
+    socket.on('connect', ()=>{
+      setOnline( true );
+    });
+    
+  }, [ socket  ]);
+  
+  useEffect(() => {
+    
+    socket.on('disconnect', ()=>{
+      setOnline( false );
+    });
+    
+  }, [ socket  ])
+
+
+  return (
+    <div className="container">
+      
+        <div className="alert" >
+          <p> 
+            Service status:
+            {
+              online 
+              ?
+              <span className="text-success" >Online</span>
+              :
+              <span className="text-danger" >Offine</span>
+            }
+          </p>
+        </div>
+
+        <h1> BandNames </h1>
+        <hr />
+
+        <div className="row" >
+          <div className="col-8" >
+            <BandList/>
+          </div>
+          <div className="col-4">
+            <BandAdd/>
+          </div>
+        </div>
+
+
+    </div>
+  );
+}
+
+export default App;
